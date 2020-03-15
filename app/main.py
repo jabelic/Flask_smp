7916e3d557eb90a4684bcf76d6c52abba7e26407
Hibiki Mizuno <hibiki990523@gmail.com>
2020-03-16 02:26:41 +0900

    add article class

--- app/main.py
+++ app/main.py
@@ -1,5 +1,6 @@
 #Flaskとrender_template（HTMLを表示させるための関数）をインポート
 from flask import Flask,render_template
+import sqlite3
 
 #Flaskオブジェクトの生成
 app = Flask(__name__)
@@ -16,7 +17,24 @@ def hello():
 def index():
     return render_template("index.html")
 
+class Article:
+    def __init__(self, id, datetime, name, article):
+        self.id = id
+        self.datetime = datetime
+        self.name = name
+        self.article = article
+    def register(self, datetime, name, article):
+        conn = sqlite3.connect('sample1.db')
+        cur = conn.cursor()
+        insert_sql = 'insert into board(datetime, name, article) values (?,?,?)'
+        users = [datetime, name, article]
+        cur.executemany(insert_sql, users)
+        conn.commit()
 
-#app.pyをターミナルから直接呼び出した時だけ、app.run()を実行する
+
+
+
+
+#main.pyをターミナルから直接呼び出した時だけ、app.run()を実行する
 if __name__ == "__main__":
     app.run(debug=True)
