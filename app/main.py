41cde1aa3792994e3abc968780165f88320be703
Hibiki Mizuno <hibiki990523@gmail.com>
2020-03-18 14:29:43 +0900

    bootstrap

--- app/main.py
+++ app/main.py
@@ -15,7 +15,7 @@ def hello():
 #「/index」へアクセスがあった場合に、「index.html」を返す
 @app.route("/index")
 def index():
-    return render_template("index.html")
+    return render_template("template/index.html")
 
 class Article:
     def __init__(self, id, datetime, name, article):
