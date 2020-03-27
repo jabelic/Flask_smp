0cd516cf3b9eae725fe8a09bd138a1c67801d40d
Hibiki Mizuno <hibiki990523@gmail.com>
2020-03-27 19:17:41 +0900

    Add code about SQLite

--- app/main.py
+++ app/main.py
@@ -1,5 +1,5 @@
 #Flaskとrender_template（HTMLを表示させるための関数）をインポート
-from flask import Flask,render_template
+from flask import Flask,render_template, request
 import sqlite3
 from datetime import datetime
 import pymysql.cursors
@@ -17,18 +17,20 @@ def hello():
     return "Hello World"
 
 
-#「/index」へアクセスがあった場合に、「thread.html」を返す
+#「/thread」へアクセスがあった場合に、「thread.html」を返す
 @app.route("/thread")
 def index():
     return render_template("thread.html", thread='hoge')
 
 class Article:
-    def __init__(self, id, datetime, name, article):
-        self.id = id
+    def __init__(self, datetime, name, article):
+        #self.id = id
         self.datetime = datetime
         self.name = name
         self.article = article
-    
+
+
+''' 
 def register(self, datetime, name, article):
     conn = sqlite3.connect('sample1.db')
     cur = conn.cursor()
@@ -36,7 +38,20 @@ def register(self, datetime, name, article):
     users = [datetime, name, article]
     cur.executemany(insert_sql, users)
     conn.commit()
+'''
 
+@app.route("/thread", methods=["POST"])
+def result():
+    time = datetime.now()
+    article = request.form['article']
+    name = request.form['name']
+    #register(time, name, article)
+    conn = sqlite3.connect('sample1.db')
+    cur = conn.cursor()
+    insert_sql = 'insert into board(datetime, name, article) values (?,?,?)'
+    users = [datetime, name, article]
+    cur.executemany(insert_sql, users)
+    conn.commit()
 
 
 
